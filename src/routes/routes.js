3e6a3608e296246f063a938ba4f459977795585f
balsamnaqa <64330336+balsamnaqa@users.noreply.github.com>
2022-10-30 11:24:45 +0400

    first commit

---
+++ src/routes/routes.js
@@ -0,0 +1,32 @@
+import ContactList from '../components/ContactList.vue';
+import AddContact from '../components/AddContact.vue';
+import EditContact from '../components/EditContact.vue';
+
+import { createRouter, createWebHistory } from 'vue-router';
+
+
+
+const routes = [
+    {
+        name: 'ContactList',
+        path: '/',
+        component: ContactList
+    },
+    {
+        name: 'AddContact',
+        path: '/add_contact',
+        component: AddContact
+    },
+    {
+        name: 'EditContact',
+        path: '/contact/edit/:id?',
+        component: EditContact
+    },
+]
+
+const  router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router;
